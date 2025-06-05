be56f9221b65343f59700fd9a99c0dce1e9337ec
prasannalakshmi5 <bonalaprasannalakshmi@gmail.com>
2025-06-05 13:51:25 +0530

    Add files via upload

---
+++ StringBuilder.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		StringBuilder StringBuilder=new StringBuilder();
+		StringBuilder.append("Hello");
+		StringBuilder.append(" ");
+		StringBuilder.append("World");
+		String result=StringBuilder.toString();
+		System.out.println("StringBuiler result:"+result);
+	}
+}
