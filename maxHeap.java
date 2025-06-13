6bdb2b6e7502ac4f06369374c7ed2f7f88b5be47
prasannalakshmi5 <bonalaprasannalakshmi@gmail.com>
2025-06-13 15:43:33 +0530

    Add files via upload

---
+++ maxHeap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args){
+	    PriorityQueue<Integer>maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+	    maxHeap.add(10);
+	    maxHeap.add(5);
+	    maxHeap.add(20);
+	    maxHeap.add(3);
+	    while(!maxHeap.isEmpty()){
+	        System.out.print(maxHeap.poll()+" ");
+	    }
+	}
+}
